c958e76562f0ffb64d2a821f3ff3fe720e157833
AzgathCore <azgath.servers@gmail.com>
2021-06-12 19:49:38 +0200

    Core/DB: Spell camp

---
+++ sql/updates/world/2021_06_12_00_world_spell.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=312370 AND `ScriptName`='spell_make_camp';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (312370, 'spell_make_camp');
+DELETE FROM `spell_script_names` WHERE `spell_id`=312372 AND `ScriptName`='spell_back_camp';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (312372, 'spell_back_camp');
