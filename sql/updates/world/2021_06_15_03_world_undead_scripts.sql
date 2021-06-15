c727524c5f0a5dce4e03993f4b66a8923fec00ef
AzgathCore <azgath.servers@gmail.com>
2021-06-16 00:15:41 +0200

    Core/SQL: Fixe quest zone undead

---
+++ sql/updates/world/2021_06_15_03_world_undead_scripts.sql
@@ -0,0 +1,26 @@
+-- Smart_scripts
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 66168 AND `source_type` = 0 AND `id` = 0 AND `link` = 0;
+
+INSERT INTO `smart_scripts` VALUES (66168, 0, 0, 0, 54, 0, 100, 512, 0, 0, 0, 0, 0, '', 80, 6616800, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Valdred - On Gossip Select - Actionlist');
+
+INSERT INTO `smart_scripts` VALUES (6616800, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, '', 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Valdred - Actionlist - Talk1');
+INSERT INTO `smart_scripts` VALUES (6616800, 9, 1, 0, 0, 0, 100, 0, 3000, 3000, 0, 0, 0, '', 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 1689.71, 1674.79, 135.675, 0.349066, 'Valdred - Action list - Go to point');
+INSERT INTO `smart_scripts` VALUES (6616800, 9, 2, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, '', 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Valdred - Actionlist - Talk2');
+INSERT INTO `smart_scripts` VALUES (6616800, 9, 3, 0, 0, 0, 100, 0, 5000, 5000, 0, 0, 0, '', 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Valdred - Actionlist - Despawn');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 66166 AND `source_type` = 0 AND `id` = 0 AND `link` = 0;
+
+INSERT INTO `smart_scripts` VALUES (66166, 0, 0, 0, 54, 0, 100, 512, 0, 0, 0, 0, 0, '', 80, 6616600, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lilian - On Gossip Select - Actionlist');
+
+INSERT INTO `smart_scripts` VALUES (6616600, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, '', 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lilian - Actionlist - Talk1');
+INSERT INTO `smart_scripts` VALUES (6616600, 9, 1, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, '', 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 1727.87, 1629.11, 118.862, 5.49784, 'Lilian - Action list - Go to point');
+INSERT INTO `smart_scripts` VALUES (6616600, 9, 2, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 0, '', 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lilian - Actionlist - Despawn');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 66167 AND `source_type` = 0 AND `id` = 0 AND `link` = 0;
+
+INSERT INTO `smart_scripts` VALUES (66167, 0, 0, 0, 54, 0, 100, 512, 0, 0, 0, 0, 0, '', 80, 6616700, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Marshal - On Gossip Select - Actionlist');
+
+INSERT INTO `smart_scripts` VALUES (6616700, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, '', 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Marshal - Actionlist - Talk1');
+INSERT INTO `smart_scripts` VALUES (6616700, 9, 1, 0, 0, 0, 100, 0, 3000, 3000, 0, 0, 0, '', 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 1753.04, 1613.1, 113.051, 2.0146, 'Marshal - Action list - Go to point');
+INSERT INTO `smart_scripts` VALUES (6616700, 9, 2, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, '', 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Marshal - Actionlist - Despawn');
