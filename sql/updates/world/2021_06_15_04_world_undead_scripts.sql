4c3ce1aa42143c7115742bcc5f3f3778aad4843e
AzgathCore <azgath.servers@gmail.com>
2021-06-16 01:11:00 +0200

    Core/SQL: Fixe quest zone undead

---
+++ sql/updates/world/2021_06_15_04_world_undead_scripts.sql
@@ -0,0 +1,5 @@
+-- Creature_queststarter
+
+DELETE FROM `creature_queststarter` WHERE `id` = 49230 AND `quest` = 24960;
+DELETE FROM `creature_queststarter` WHERE `id` = 38895 AND `quest` = 24960;
+DELETE FROM `creature_queststarter` WHERE `id` = 49231 AND `quest` = 24960;
