e01b26ba1f7b646b943d1364298a9f26407dfd5b
AzgathCore <azgath.servers@gmail.com>
2021-06-15 22:58:39 +0200

    Core/DB: Misc fixes

---
+++ sql/updates/world/2021_06_15_00_world_version.sql
@@ -0,0 +1,1 @@
+UPDATE `version` SET `db_version` = 'AZDB 837' WHERE `core_version` = 'AzgathCore rev. bd4e718435cf 2021-06-12 21:08:13 +0200 (AzgathCoreBFA branch) (Win64, Release, Static)';
---
+++ sql/updates/world/2021_06_15_02_world_undead_scripts.sql
@@ -0,0 +1,3 @@
+-- Creature_template
+
+UPDATE `creature_template` SET `npcflag` = 0 WHERE `entry` = 49340;
